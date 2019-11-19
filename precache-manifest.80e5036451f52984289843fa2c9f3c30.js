d6934e8484a35565f9dd0df9f02a51b40f95eb3b
Romain <romain.mard@gmail.com>
2019-11-19 10:09:28 +0100

    Updates

---
+++ precache-manifest.80e5036451f52984289843fa2c9f3c30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07fd05f576cd669ecc11704666e195d",
+    "url": "/isport/index.html"
+  },
+  {
+    "revision": "94b5fef794edda20911b",
+    "url": "/isport/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "74e263539486c05f8e6d",
+    "url": "/isport/static/js/2.26bcce0e.chunk.js"
+  },
+  {
+    "revision": "94b5fef794edda20911b",
+    "url": "/isport/static/js/main.8b4fcc6d.chunk.js"
+  },
+  {
+    "revision": "02f51dc8999b14f61e16",
+    "url": "/isport/static/js/runtime-main.6d4575f0.js"
+  }
+]);
